04d9871ebafcc63b554b2f862b7d8dd09b185a3c
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-08-24 03:05:46 -0500

    add aidoku exports

--- packages/mangabackupconverter_cli/lib/mangabackupconverter_lib.dart
+++ packages/mangabackupconverter_cli/lib/mangabackupconverter_lib.dart
@@ -1,1 +1,8 @@
 export 'src/converter.dart';
+export 'src/exceptions/aidoku_exception.dart';
+export 'src/formats/aidoku/aidoku_backup.dart';
+export 'src/formats/aidoku/aidoku_backup_chapter.dart';
+export 'src/formats/aidoku/aidoku_backup_history.dart';
+export 'src/formats/aidoku/aidoku_backup_library_manga.dart';
+export 'src/formats/aidoku/aidoku_backup_manga.dart';
+export 'src/formats/aidoku/aidoku_backup_track_item.dart';
